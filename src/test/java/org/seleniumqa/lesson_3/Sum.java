0b736aca0bb83e83ce23896a9e1745f0295bd768
arkhipova <aniaarhipova@gmail.com>
2015-02-25 20:25:32 +0200

    Sum funct

---
+++ src/test/java/org/seleniumqa/lesson_3/Sum.java
@@ -0,0 +1,26 @@
+package org.seleniumqa.lesson_3;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by admin on 25.02.2015.
+ */
+public class Sum {
+
+    @Test
+    private static void valid_Sum(){
+        Assert.assertEquals(sum(2,4),6,"Sum passed");
+
+    }
+
+    static public int sum(int a, int b){
+        int c;
+        c = a+b;
+        return c;
+    }
+
+}
